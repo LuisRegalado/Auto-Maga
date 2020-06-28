6e2e2c1243df80d01d5fc6e4b6e0de001945974e
CesarContreras <cesar.contreras.fiscal@gmail.com>
2020-06-28 04:48:45 -0500

    Responsive design is ready boys

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthService} from '../auth/services/auth.service';
+import {Observable}from 'rxjs'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [AuthService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public user$: Observable <any> = this.authS.afAuth.user;
+  
+  constructor(private authS: AuthService) { }
 
   ngOnInit(): void {
   }
